d52476938d7f0ed0e73a1bb678c51ce89d9c50f1
hp <hp@DESKTOP-5L9BE68>
2024-12-01 10:53:35 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("hi Venkat");
+            int venkatAge = 23;
+            double doubleAge = 23.345;
+            char letters = 'a';
+            string sentence = "hai";
+            Console.WriteLine(venkatAge);
+            Console.WriteLine(doubleAge);
+            Console.WriteLine(letters);
+            Console.WriteLine(sentence);
+
+        }
+
+        // retrns age of venkat
+        public static int age()
+        {
+            return 23;
+        }
+
+
+    }
+}
